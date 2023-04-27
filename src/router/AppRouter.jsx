7e5f9d4354e58d9b4caf690d5660fc6068d10fc9
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-26 19:15:37 -0700

    Created Router Component

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Marvel, DC } from "../heroes/pages"
+import { Login } from "../auth/pages/Login"
+import { Navbar } from "../ui/components/Navbar"
+
+export const AppRouter = () => {
+  return (
+	<>
+		<Navbar />
+
+		<Routes>
+			<Route path="marvel" element={<Marvel />} />
+			<Route path="dc"  element={<DC />}/>
+			<Route path="login"  element={<Login />}/>
+
+			<Route path="/*"  element={<Navigate to={"/marvel"} />}/>
+		</Routes>
+	</>
+  )
+}
