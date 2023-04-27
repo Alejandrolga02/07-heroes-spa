7025784816e269ceccb98a9f228e3bd59e4fcec5
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-26 22:28:46 -0700

    Added class active if you are in the route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Marvel, DC } from "../heroes/pages"
-import { Login } from "../auth/pages/Login"
-import { Navbar } from "../ui/components/Navbar"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Marvel, DC } from "../heroes/pages";
+import { Login } from "../auth/pages/Login";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
-  return (
-	<>
-		<Navbar />
+	return (
+		<>
+			<Navbar />
 
-		<Routes>
-			<Route path="marvel" element={<Marvel />} />
-			<Route path="dc"  element={<DC />}/>
-			<Route path="login"  element={<Login />}/>
+			<Routes>
+				<Route path="marvel" element={<Marvel />} />
+				<Route path="dc" element={<DC />} />
+				<Route path="login" element={<Login />} />
 
-			<Route path="/*"  element={<Navigate to={"/marvel"} />}/>
-		</Routes>
-	</>
-  )
-}
+				<Route path="/*" element={<Navigate to={"/marvel"} />} />
+			</Routes>
+		</>
+	);
+};
