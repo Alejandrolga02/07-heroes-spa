460b71e2dccde8b77ef8c2d147f820ed951a727e
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-28 20:39:18 -0700

    Created logout function

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -6,30 +6,49 @@ import { AuthContext, authReducer } from "./";
 import { types } from "../types";
 
 const init = () => {
+	const user = JSON.parse(localStorage.getItem('user'));
+
 	return {
-		logged: false,
+		logged: !!user,
+		user
 	};
 };
 export const AuthProvider = ({ children }) => {
 	const [authState, dispatch] = useReducer(authReducer, {}, init);
 
 	const login = (name) => {
+		const user = {
+			id: 'ABC',
+			name
+		}
+		
 		const action = {
 			type: types.login,
-			payload: {
-				name,
-			},
+			payload: user,
 		};
+		
+		localStorage.setItem('user', JSON.stringify(user))
 
 		dispatch(action);
 	};
+
+	const logout = () => {
+		localStorage.removeItem('user');
+
+		const action = {
+			type: types.logout,
+		}
+
+		dispatch(action);
+	}
 
 	return (
 		<AuthContext.Provider value={{ 
 			...authState, 
 
 			// Methods
-			login 
+			login,
+			logout
 		}}>
 			{children}
 		</AuthContext.Provider>
